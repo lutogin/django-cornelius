bba941be7afc05154d58c8496862a37d3bc30c99
Andrey Lutogin <lutogin@trueconf.ru>
2019-09-28 21:13:07 +0300

    fix mail tmpl, fix include in url

--- cart/urls.py
+++ cart/urls.py
@@ -1,13 +1,13 @@
 from django.urls import re_path
-from cart import views
+from cart.views import *
 
 app_name = 'cart'
 
 urlpatterns = [
-    re_path(r'^remove/(?P<product_id>\d+)/$', views.cart_remove, name='CartRemove'),
-    re_path(r'^add/(?P<product_id>\d+)/$', views.cart_add, name='CartAdd'),
-    re_path(r'^update/$', views.cart_update, name='CartUpdate'),
-    re_path(r'^get-cart-api/$', views.get_cart_api, name='GetCartApi'),
-    re_path(r'^submit/$', views.cart_submit, name='CartSubmit'),
-    re_path(r'^$', views.cart_detail, name='CartDetail'),
+    re_path(r'^remove/(?P<product_id>\d+)/$', cart_remove, name='CartRemove'),
+    re_path(r'^add/(?P<product_id>\d+)/$', cart_add, name='CartAdd'),
+    re_path(r'^update/$', cart_update, name='CartUpdate'),
+    re_path(r'^get-cart-api/$', get_cart_api, name='GetCartApi'),
+    re_path(r'^submit/$', cart_submit, name='CartSubmit'),
+    re_path(r'^$', cart_detail, name='CartDetail'),
 ]
--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from shop.views import index, category_products, product_detail
+from shop.views import *
 
 app_name = 'shop'
 
