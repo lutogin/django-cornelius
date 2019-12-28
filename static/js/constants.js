7af1ba5149cb526bf41d4bb2c828ca393ae56fe3
lutogin <lutogin@gmail.com>
2019-12-28 23:31:26 +0200

    fix JS add to cart

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
-const HOST = '0.0.0.0:8000';
-const PROTOCOL = 'http';
+const HOST = '127.0.0.1:8000';
+const PROTOCOL = 'https';
 const HOST_URL = `${PROTOCOL}://${HOST}`;
 const ENGRAVING_PRICE = 500;
