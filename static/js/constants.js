04a6005b1dacec98dc75fe9319e4194f109f0ae5
lutogin <lutogin@gmail.com>
2019-12-29 14:32:31 +0200

    Fix link in js+

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,4 +1,4 @@
-const HOST = '127.0.0.1:8000';
-const PROTOCOL = 'http';
+const HOST = 'cornelius-dom.art';
+const PROTOCOL = 'https';
 const HOST_URL = `${PROTOCOL}://${HOST}`;
 const ENGRAVING_PRICE = 500;
