58a50a9d6697d149c64bd124018248cd7f43aa4a
lutogin <lutogin@gmail.com>
2019-12-30 22:22:35 +0200

    FixUrls

--- shop/urls.py
+++ shop/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     re_path(r'^engraving/?$', engravings_page, name='engraving'),
     re_path(r'^about/?$', about_page, name='about'),
     re_path(r'^category/(?P<category_slug>[a-z]{3,20})', category_products, name='category'),
-    re_path(r'^product/(?P<p_id>[0-9]{1,4})', product_detail, name='product'),
+    re_path(r'^product/(?P<p_id>\d)', product_detail, name='product'),
 ]
