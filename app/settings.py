579b1ffac933522912d8b92be46000e1ebc641ad
lutogin <lutogin@gmail.com>
2019-12-30 22:56:17 +0200

    REVERT WIP

--- app/settings.py
+++ app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hekto.v8@gmail.com'
